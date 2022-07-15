76fe262aeff782a782e8241e968174c2244f3ffc
mw <534203700@qq.com>
2022-07-15 16:31:02 +0800

    自定义hashmap方法

--- src/main/java/map/Test.java
+++ src/main/java/map/Test.java
@@ -6,19 +6,21 @@ public class Test {
 
     public static void main(String[] args) {
 
-        FourthlyHashMap map = new FourthlyHashMap();
+        ThirdHashMap map = new ThirdHashMap();
         for(int i=0;i<100;i++){
             map.put("测试"+i,"测试map"+i);
         }
         System.out.println(map.size());
+        map.remove("测试99");
         for(int i= 0;i<100;i++){
             System.out.println(map.get("测试"+i));
         }
 
-        FourthlyHashMap map1 = new FourthlyHashMap();
+        ThirdHashMap map1 = new ThirdHashMap();
         map1.put("测试2","我是测试2");
         map1.put("测试2","我又被添加了");
         System.out.println(map1.get("测试2"));
+
     }
 
 
--- src/main/java/map/ThirdHashMap.java
+++ src/main/java/map/ThirdHashMap.java
@@ -1,14 +1,13 @@
 package map;
 
-public class ThirdHashMap<K,V> {
+public class ThirdHashMap<K, V> {
 
-    class Node<K,V>{
-
+    class Node<K, V> {
         private K key;
 
         private V value;
 
-        private Node<K,V> next;
+        private Node<K, V> next;
 
         public Node(K key, V value) {
             this.key = key;
@@ -22,100 +21,113 @@ public class ThirdHashMap<K,V> {
         }
     }
 
-    final int DEFAULT_CAPACITY = 16;
-
-    final float LOAD_FACTOR = 0.75f;
+    final int DEFAULT_CA = 16;
+    final float DEFAULT_LOAD_FA = 0.75f;
 
     private int size;
 
-    Node<K,V>[] buckets;
+    private Node<K, V>[] bucket;
 
-    public ThirdHashMap(){
-        buckets = new Node[DEFAULT_CAPACITY];
+    public ThirdHashMap(int size) {
+        bucket = new Node[size];
         size = 0;
     }
 
-    public ThirdHashMap(int capacity){
-        buckets = new Node[capacity];
+    public ThirdHashMap() {
+        bucket = new Node[DEFAULT_CA];
+        size = 0;
     }
 
-    private int getIndex(K key, int length){
+    public int size() {
+        return size;
+    }
+
+    private int getIndex(K key, int length) {
         int hashCode = key.hashCode();
-        int index = hashCode%length;
-        return Math.abs(index);
+        int index = hashCode % length;
+        return index;
     }
 
-
-    public void put(K key , V value){
-        if(size >= buckets.length * LOAD_FACTOR){
-           resize();
+    public V get(K key) {
+        int index = getIndex(key, bucket.length);
+        Node<K, V> node = bucket[index];
+        if (node == null) {
+            return null;
         }
-        putVal(key,value,buckets);
+        while (node != null) {
+            if (node.key.hashCode() == key.hashCode() &&
+                    (key == node.key || key.equals(key))) {
+                return node.value;
+
+            }
+            node = node.next;
+        }
+        return null;
     }
 
-    /**
-     * 扩容方法
-     */
-    private void resize(){
-        Node<K,V>[] newBuckets = new Node[buckets.length * 2];
-        rehash(newBuckets);
-        buckets = newBuckets;
-
+    public void put(K key, V value) {
+        if (size >= bucket.length * DEFAULT_LOAD_FA) {
+            resize();
+        }
+        putValue(key, value, bucket);
     }
 
-    private void putVal(K key , V value , Node<K , V>[] table){
-        int index = getIndex(key , table.length);
-        Node node = table[index];
-        if(node == null){
-            table[index] = new Node<K, V>(key , value);
-            size ++;
+    public void remove(K key){
+        int index = getIndex(key, bucket.length);
+        Node<K, V> node = bucket[index];
+        if (node == null) {
             return;
         }
-        while (node != null){
-            if((node.key.hashCode() == key.hashCode()) && (node.key == key || node.key.equals(key))){
+        while (node != null) {
+            if (node.key.hashCode() == key.hashCode() &&
+                    (key == node.key || key.equals(key))) {
+
+                bucket[index]=null;
+                return;
+            }
+            node = node.next;
+        }
+    }
+
+    private void resize() {
+        Node<K, V>[] newBucket = new Node[bucket.length * 2];
+        rehash(newBucket);
+        bucket = newBucket;
+    }
+
+    private void rehash(Node<K, V>[] newBucket) {
+        size = 0;
+        for (int i = 0; i < bucket.length; i++) {
+            Node<K, V> node = bucket[i];
+            if (node == null) {
+                continue;
+            }
+            while (node != null) {
+                putValue(node.key, node.value, newBucket);
+                node=node.next;
+            }
+        }
+    }
+
+    private void putValue(K key, V value, Node<K, V>[] tables) {
+        int index = getIndex(key, tables.length);
+        Node node = tables[index];
+        if (node == null) {
+            tables[index] = new Node<K, V>(key, value);
+            size++;
+            return;
+        }
+        while (node != null) {
+            if (node.key.hashCode() == key.hashCode() &&
+                    (key == node.key || key.equals(key))) {
                 node.value = value;
                 return;
             }
             node = node.next;
         }
-        Node newNode = new Node(key , value , table[index]);
-        table[index] = newNode;
-        size ++ ;
-    }
+        Node<K, V> newNode = new Node<K, V>(key, value, tables[index]);
+        tables[index] = newNode;
+        size++;
 
-
-
-    private void rehash(Node<K, V>[] newBuckets){
-        size = 0;
-        for(int i = 0; i<buckets.length; i++){
-            if(buckets[i] == null){
-                continue;
-            }
-            Node<K, V > node= buckets[i];
-            while(node != null){
-                putVal(node.key,node.value,newBuckets);
-                node = node.next;
-            }
-        }
-    }
-
-
-    public V get(K key){
-        int index = getIndex(key,buckets.length);
-        if(buckets[index] == null){
-            return  null;
-        }
-        Node<K , V> node = buckets[index];
-        while (node !=null){
-            if((node.key.hashCode() == key.hashCode()) && (node.key == key || node.key.equals(key))){
-                return node.value;
-            }
-            node = node.next;
-        }
-        return  null;
-    }
-
-    public int size(){
-        return size;
     }
 }
